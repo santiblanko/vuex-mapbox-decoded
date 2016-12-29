dc535c1403c04762db227b0dea06279c5bd09a95
santiago <santiago.blanco.vilchez@gmail.com>
2016-12-29 11:19:12 -0500

    added builder route for testing

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import GeoBirdApp from './components/geobird/App'
 import store from './store/index'
+import builder from './components/orch/builder'
+
 Vue.use(VueRouter)
 const routes = [
-  { path: '/geobird', component: GeoBirdApp }
+  { path: '/geobird', component: GeoBirdApp },
+  { path: '/builder', component: builder }
 ]
 
 const router = new VueRouter({
@@ -16,7 +19,8 @@ const router = new VueRouter({
 new Vue({
   router,
   components: {
-    GeoBirdApp
+    GeoBirdApp,
+    builder
   },
   store
 }).$mount('#app')
